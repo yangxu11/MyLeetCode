85471908055a7eff4d20563ce383f352bb4bce9f
phenix <1447714360@qq.com>
2019-04-16 17:42:39 +0800

    commit

---
+++ Q292.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * Nim游戏
+ *
+ * @author yx
+ * @create 2019-04-16  17:41
+ **/
+public class Q292 {
+    public boolean canWinNim(int n) {
+        if(n%4==0) return false;
+        else return true;
+    }
+}
