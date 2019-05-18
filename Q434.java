974868e893ac9e2f7cc9f5a3eb942ffcc8804b3f
phenix <1447714360@qq.com>
2019-05-18 18:51:10 +0800

    commit

--- Q434.java
+++ Q434.java
@@ -1,22 +1,17 @@
 package leetcode;
 //字符串中的单词数
 public class Q434 {
-    //注意连续的空格，和结尾处的空格
+
     public int countSegments(String s) {
-        if(s.length()==0) return 0;
-        int sign=0;
-        int count=0;
-        for(int i=0 ; i<s.length() ; i++){
-            char c = s.charAt(i);
-            if(c==' '){
-                if(sign==1){
-                    count++;
-                    sign=0;
-                }
-            } else {
-                sign=1;
+        String str = s.trim();
+        if(str.length()==0) return 0;
+        String[] strs = str.split(" ");
+        int count =0;
+        for(String i : strs){
+            if(i.trim().length()!=0){
+                ++count;
             }
         }
-        return s.charAt(s.length()-1)==' ' ? count : count+1;
+        return count;
     }
 }
