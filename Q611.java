a5d0920869bcbc69d0ad7fc20683abbebc9d82b4
yx <18811434248@163.com>
2019-06-18 20:33:18 +0800

    commit

--- Q611.java
+++ Q611.java
@@ -4,24 +4,26 @@ import java.util.Arrays;
 
 //有效三角形的个数
 public class Q611 {
-    //128ms 38%
-    //暴力解法
-    //先排序，遍历寻找前两个的和，然后判断第三个是否<前两个之和
     public int triangleNumber(int[] nums) {
-        int result = 0;
+        if(nums.length<3) return 0;
+
         Arrays.sort(nums);
-        for(int i=0 ; i<nums.length-2 ; i++){
-            for(int j=i+1 ; j<nums.length-1 ; j++){
-                int sum = nums[i] + nums[j];
-                for(int k=j+1 ; k<nums.length ; k++){
-                    if(sum>nums[k]){
-                        result++;
-                    } else {
-                        break;
-                    }
+
+        int res = 0;
+
+        for(int i=nums.length-1 ; i>=2 ; i--){
+            int start = 0;
+            int end = i-1;
+            while(start<end){
+                if(nums[start]+nums[end]>nums[i]){
+                    res += end-start;
+                    end--;
+                } else{
+                    start++;
                 }
             }
         }
-        return result;
+
+        return res;
     }
 }
